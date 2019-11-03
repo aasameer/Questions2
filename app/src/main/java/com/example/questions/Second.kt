cca4d7fd8b489334a1eef65022504331d5756b43
Ahmed Helmi <aasameer@hotmail.com>
2019-11-03 16:44:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/questions/Second.kt
@@ -0,0 +1,12 @@
+package com.example.questions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Second : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.second)
+    }
+}
